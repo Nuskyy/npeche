3ba5ea04d3f8b0212c794d372b1699bd344956db
Nuskyy <64265708+Nuskyy@users.noreply.github.com>
2021-07-06 17:59:29 +0200

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+('canne', 'Canne à peche', -1)
+('poisson', 'Poissons', -1)
+('poissonbarquette', 'Barquette de Poissons', -1)
+('barquette', 'Barquette', -1);
