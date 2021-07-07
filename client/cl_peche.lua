839994f5ad07e3d1b1808396ef4eb38875d402d8
Nuskyy <64265708+Nuskyy@users.noreply.github.com>
2021-07-07 06:51:35 +0200

    Update cl_peche.lua

--- client/cl_peche.lua
+++ client/cl_peche.lua
@@ -10,36 +10,56 @@ end)
 
 RegisterNetEvent('n:startpeche')
 AddEventHandler('n:startpeche', function()
-    local ped = GetPlayerPed(-1)
-    if IsEntityInWater(ped) then 
-        TaskStartScenarioInPlace(GetPlayerPed(-1), "WORLD_HUMAN_STAND_FISHING", 0, true)
-        TriggerServerEvent('n:fishing')
-    else 
-        ESX.ShowNotification('Tu es loin de l\'eau')
-    end
+    peche()
 end)
 
-local vente = {x = ,y = ,z = }
+RegisterNetEvent('n:startbarquette')
+AddEventHandler('n:startbarquette', function()
+    TriggerServerEvent('n:barquette')
+end)
+function peche()
+    local ped = GetPlayerPed(-1)
+    local pProps=GetHashKey("prop_fishing_rod_01")
+    local time = 12000
+    if pechetime and pechetime > GetGameTimer() then 
+		if notifCass then 
+			RemoveNotification(notifCass) 
+		end
+		notifCass = ESX.ShowNotification(string.format("~r~Veuillez patienter encore %s seconde(s) avant de re-utiliser votre canne à pêche.", math.floor((pechetime - GetGameTimer()) / 1000))) return 
+	end
+
+	local treeeeTime = 0 + time
+	pechetime = GetGameTimer() + treeeeTime
+    if IsEntityInWater(ped) then 
+        TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_STAND_FISHING", 0, true)
+        Citizen.Wait(12000)
+        TriggerServerEvent('n:fishing')
+    elseif not IsEntityInWater(ped) then 
+        ESX.ShowNotification('Tu es loin de l\'eau')
+    end 
+    ClearPedTasks(GetPlayerPed(-1))
+    Citizen.Wait(2000)
+end 
+
+local vente = { {x = -68.52, y = 213.801 , z = 97.22 } }
 
 Citizen.CreateThread(function()
-    while true do 
+    while true do
         Citizen.Wait(0)
-        local ped = GetPlayerPed(-1)
-        ESX.ShowNotification('Appuyez sur ~g~E~s~ pour commencer à vendre')
-        if IsEntityInZone(ped, vente) and IsControlJustPressed(1, 38) then 
-            Citizen.Wait(10)
-            TriggerServerEvent('n:ventepeche')
-            ESX.ShowNotification('Appuyez sur ~g~E~s~ pour arreter de vendre')
-            if IsControlJustPressed(1, 38) then 
-                TriggerServerEvent('n:stopvente')
-            end 
-        else 
-            ESX.ShowNotification('Tu n\'as rien a vendre')
-        end 
-    end 
-end)
+        for k in pairs(vente) do
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, vente[k].x, vente[k].y, vente[k].z)
 
-local garagepeche = {x = ,y = ,z = }
+            if dist <= 2 then
+                ESX.ShowHelpNotification("Appuyez sur ~INPUT_PICKUP~ pour vendre vos ~b~poissons~s~")
+				if IsControlJustPressed(1,38) then 
+					TriggerServerEvent('n:ventepeche')
+				end
+            end
+		end
+    end
+end) 
+local garagepeche = { {x = -3236.01 ,y = 952.411 ,z = 13.7 } }
 
 
 local garagepecheur = {
@@ -54,7 +74,7 @@ local garagepecheur = {
 			if btn == "Burrito" then
 				SetVehicleWindowTint(vehicle, 5)
 				Citizen.Wait(1)
-				spawnCar("burrito")
+				spawnCar("burrito3")
 				CloseMenu(garagepecheur)
 		end
 	end,
@@ -78,7 +98,7 @@ function spawnCar(car)
     end
 
     local x, y, z = table.unpack(GetEntityCoords(PlayerPedId(), false))
-    local vehicle = CreateVehicle(car, 294.62, -607.61, 43.11, 72.33, true, false)
+    local vehicle = CreateVehicle(car, -3231.83, 940.10, 13.53, 288.7, true, false)
     SetPedIntoVehicle(PlayerPedId(), vehicle, -1)
     
     SetEntityAsNoLongerNeeded(vehicle)
@@ -92,7 +112,6 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         for k in pairs(garagepeche) do
-
             local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
             local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, garagepeche[k].x, garagepeche[k].y, garagepeche[k].z)
 
@@ -105,3 +124,72 @@ Citizen.CreateThread(function()
 		end
     end
 end) 
+
+local del = {
+    {x = -3234.27 , y = 946.58, z = 13.73}
+}
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+		for k in pairs(del) do
+			
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, del[k].x, del[k].y, del[k].z)
+
+            if dist <= 3 and IsPedInAnyVehicle(PlayerPedId(-1), false) then
+                ESX.ShowHelpNotification("Appuyez sur ~INPUT_PICKUP~ pour rentrer votre à ~b~vehicule~s~ dans le ~b~garage~s~")
+				if IsControlJustPressed(1,38) then 			
+					TriggerEvent('esx:deleteVehicle')
+				end
+            end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+    Citizen.Wait(50)
+	for i=1, 1 do
+		
+		local blip = AddBlipForCoord(vente[i].x, vente[i].y, vente[i].z)
+		SetBlipSprite (blip, 1)
+		SetBlipDisplay(blip, 4)
+		SetBlipColour (blip, 3)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString('Vente de Poissons')
+		EndTextCommandSetBlipName(blip)
+	end
+
+end)
+
+Citizen.CreateThread(function()
+	Citizen.Wait(50)
+	for i=1, 1 do
+		
+		local blip = AddBlipForCoord(garagepeche[i].x, garagepeche[i].y, garagepeche[i].z)
+		SetBlipSprite (blip, 1)
+		SetBlipDisplay(blip, 4)
+		SetBlipColour (blip, 3)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString('Garage Pecheur')
+		EndTextCommandSetBlipName(blip)
+	end
+
+end)
+
+Citizen.CreateThread(function()
+    local hash = GetHashKey("cs_jimmyboston")
+    while not HasModelLoaded(hash) do
+    RequestModel(hash)
+    Wait(20)
+    end
+    ped = CreatePed("PED_TYPE_CIVFEMALE", "cs_jimmyboston", -3236.01 , 952.411 , 12.2 , 275.1554794, false, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    SetEntityInvincible(ped, true)
+    FreezeEntityPosition(ped, true)
+end)
